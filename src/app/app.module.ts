209cff552a7d4aa9e22654250bc3091172fa5ded
ewice <henning.loewe@hotmail.de>
2021-12-19 16:26:56 +0100

    remove missing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    MapComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
